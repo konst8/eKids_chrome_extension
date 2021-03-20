321d96bafcd47c952a40a2644f7624e33412dad6
Konstantin Mishur <kmishur@its.jnj.com>
2021-03-20 10:58:22 +0300

    Lesson 4: Add click event, add test document, run only on local files

--- content.js
+++ content.js
@@ -1,6 +1,19 @@
-// Create a new element and put it
-// after the main heading.
-
 let heading2 = document.createElement('h2');
 heading2.innerHTML = 'Hello Planet!!!';
+
+heading2.style.cursor = 'pointer';
+
+heading2.addEventListener(
+  'click',
+  (event) => {
+    console.log('event info', event);
+    let allElements = document.querySelectorAll('*');
+    allElements.forEach(
+      element => {
+        element.style.backgroundColor = 'orange';
+      }
+    );
+  }
+);
+
 document.querySelector('h1').after(heading2);
