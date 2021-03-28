4538c27b5659d90bef02e844f0348c0f9456cb28
Konstantin Mishur <kmishur@its.jnj.com>
2021-03-28 11:11:30 +0300

    Add test files

---
+++ test.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  let heading2 = document.createElement('h2');
+  heading2.innerHTML = 'Hello Planet!!!';
+  
+  heading2.style.cursor = 'pointer';
+  
+  heading2.addEventListener(
+    'click',
+    (event) => {
+      console.log('event info', event);
+      let allElements = document.querySelectorAll('*');
+      allElements.forEach(
+        element => {
+          element.style.backgroundColor = 'orange';
+        }
+      );
+    }
+  );
+  
+  document.querySelector('h1').after(heading2);
+
+});
