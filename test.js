30655c631b4c4493cdb40d58297c64e88cddc5ef
Konstantin Mishur <kmishur@its.jnj.com>
2021-04-04 11:06:39 +0300

    Add test element

--- test.js
+++ test.js
@@ -1,40 +1,63 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   let allElements = document.querySelectorAll('*');
-  let headings = document.querySelectorAll('h3');
-  let heading2 = document.createElement('h2');
-
-  heading2.innerHTML = 'Hello Planet!!!';
-  heading2.style.cursor = 'pointer';
-  
-  heading2.addEventListener(
+  let h3 = document.createElement('h3');
+  h3.innerHTML = 'blue';
+  h3.style.color = 'blue';
+  h3.style.cursor = 'pointer';
+  h3.addEventListener(
     'click',
-    (event) => {
+    event => {
+      let color = event.target.style.color;
       allElements.forEach(
         element => {
-          element.style.backgroundColor = 'orange';
+          element.style.backgroundColor = 'blue';
         }
       );
     }
   );
+  document.body.append(h3);
+
+
+
+
+
+
+
+  // let headings = document.querySelectorAll('h3');
+  // let heading2 = document.createElement('h2');
+
+  // heading2.innerHTML = 'Hello Planet!!!';
+  // heading2.style.cursor = 'pointer';
   
-  document.querySelector('h1').after(heading2);
+  // heading2.addEventListener(
+  //   'click',
+  //   (event) => {
+  //     allElements.forEach(
+  //       element => {
+  //         element.style.backgroundColor = 'orange';
+  //       }
+  //     );
+  //   }
+  // );
+  
+  // document.querySelector('h1').after(heading2);
 
   // Change background color of all elements on
   // clicking h3 heading.
 
-  headings.forEach(heading => {
-    heading.addEventListener(
-      'click',
-      event => {
-        let color = event.target.style.color;
-        allElements.forEach(
-          element => {
-            element.style.backgroundColor = color;
-          }
-        );
-      }
-    )
-  });
+  // headings.forEach(heading => {
+  //   heading.addEventListener(
+  //     'click',
+  //     event => {
+  //       let color = event.target.style.color;
+  //       allElements.forEach(
+  //         element => {
+  //           element.style.backgroundColor = color;
+  //         }
+  //       );
+  //     }
+  //   )
+  // });
 
 });
