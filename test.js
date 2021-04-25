c43732216105066f627c9dfc48f234cb7e3b5ffd
Konstantin Mishur <kmishur@its.jnj.com>
2021-04-25 11:13:23 +0300

    Show object examples

--- test.js
+++ test.js
@@ -1,4 +1,34 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+  // Create object
+
+  let obj = {
+    key1: 'val1',
+    key2: 'val2',
+    key3: {
+      a: 'a',
+      b: 'b',
+    }
+  }
+
+  // Get object value
+
+  obj.key1; // 'val1'
+
+
+  // Get deep object value
+
+  obj.key3.a // 'a'
+
+  // Use unsupported symbols in object keys
+
+  let obj = {
+    'key-1': 'val1',
+    key2: 'val2',
+  }
+
+  // Get an item with unsupported key
+
+  obj[key-1]; // 'val1'
 
 });
